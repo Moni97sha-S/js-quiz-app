723d73c834613915626f616c92b55c2db043a11b
Moni97sha-S <129970757+Moni97sha-S@users.noreply.github.com>
2023-06-21 00:38:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-console.log("hi");
 const xhttp = new XMLHttpRequest();
 xhttp.open('GET','https://5d76bf96515d1a0014085cf9.mockapi.io/quiz');
 xhttp.send();
@@ -12,30 +11,39 @@ function onReadyStateChange(e){
        // console.log(datum);
       loadUI(datum);
     //   console.log(secTag);
+    
     }
 }
 function loadUI(data){
   data.forEach((val, ind, arr)=>{
     const secQuiz = document.querySelector(".quiz");
      const quizContDiv = document.querySelector(".quizContainer");
- 
+    //  const btn = document.createElement('button');
+    //  btn.classList.add("btn");
+    //  btn.classList.add("btn-primary");
+    //  btn.value="Submit";
+    //  btn.type="Submit";
+    //  secQuiz.append(btn);
+    
+
     //Questions
     const quesDiv = document.createElement('div')
     quesDiv.classList.add("quesContainer");
     const p = document.createElement('p');
     p.classList.add("questions")
-    p.innerText = `Q${val.id}.` + val.question;
+    p.innerText = `Q${val.id}. ` + val.question;
     quesDiv.append(p);
     
     quizContDiv.append(quesDiv);
-   
+    
     //quesDiv.insertAjacentHTML('afterbegin', `<hr />`)
   
     // Options
     val.options.forEach(option => {
       const optItem = optionCont(option, ind );
       quesDiv.append(optItem);
-      console.log(quesDiv);
+      
+    //   console.log(quesDiv);
     //   const ansTextLabel = optItem.querySelector('label');
     //   const scoreCardDiv = document.querSelector('.scoreCard')
       
@@ -49,24 +57,45 @@ function loadUI(data){
     //   });
     });
     
+
     $(quesDiv).append($("<hr />"));
     // quizcont.append(divOpt);
     //  
-    secQuiz.append(quizContDiv);
- })
-}
 
-function optionCont(option, ind){
+    secQuiz.append(quizContDiv);
+/***********************************************************
+    console.log(val.options.length)
+    indexes= val.options.length*5
+    for(let i=0; i<indexes; i++){
+        $('input').attr('id', `${indexes}`)
+    }
+     */
+
+    function optionCont(option, ind){
     const divOpt = document.createElement("div");
     divOpt.classList.add("option");
     divOpt.insertAdjacentHTML('afterbegin', `
-      <label for="radBtns${ind}">
-      <input type="radio" id="radBtns${ind}" name="Choice_${ind}"></input>
+      <label>
+      <input type="radio" name="Choice_${ind}"></input>
       ${option}
       </label>
     `);
   return divOpt;
 }
+ })
+}
+
+// function optionCont(option, ind){
+//     const divOpt = document.createElement("div");
+//     divOpt.classList.add("option");
+//     divOpt.insertAdjacentHTML('afterbegin', `
+//       <label for="radBtns${ind}">
+//       <input type="radio" id="radBtns${ind}" name="Choice_${ind}"></input>
+//       ${option}
+//       </label>
+//     `);
+//   return divOpt;
+// }
 
 
 
