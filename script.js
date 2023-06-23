cd74b39b5caa2dd3f92e2846fb9e91a52cb91572
Moni97sha-S <129970757+Moni97sha-S@users.noreply.github.com>
2023-06-23 23:28:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,143 +1,128 @@
 
 // console.log("hi");
-const xhttp = new XMLHttpRequest();
-xhttp.open('GET','https://5d76bf96515d1a0014085cf9.mockapi.io/quiz');
-xhttp.send();
-xhttp.addEventListener('readystatechange', onReadyStateChange);
-
-function onReadyStateChange(e){
-    e.preventDefault();
-    if(e.target.readyState === 4 && e.target.status === 200){
-      const datum = JSON.parse(e.target.responseText);
-       // console.log(datum);
-      loadUI(datum);
-      const secQuiz = document.querySelector(".quiz");
-      const form = document.querySelector(".quiz_form");
-      form.append(quizContDiv)
-      secQuiz.append(form);
-    }
-}
-
-function loadUI(data){
-  data.forEach((val, ind, arr)=>{
-    
-     const quizContDiv = document.querySelector(".quizContainer");
-     // console.log(val.id)
-    
-    //Questions
-    const quesDiv = document.createElement('div')
-    quesDiv.classList.add("quesContainer");
-    const p = document.createElement('p');
-    p.classList.add("questions")
-    p.textContent = `Q${val.id}. ${val.question}` ;
-    quesDiv.append(p);
-    quizContDiv.append(quesDiv);
-    // console.log(data[0].options[0])
-    /*quesDiv.insertAjacentHTML('afterbegin', `<hr />`) */
-    // console.log(val.options[0])
-  //console.log(data[ind].options[0])
-   /* for(let i=0; i<data[ind].id.length;i++){
-      for(let j=0; j<options[i].length;j++){
-         // console.log(val.options[i]);
-        console.log(data[i].options[j])
-        $("input").val(data[i].options[j])
-      }
-    } */
-    // Options
-    val.options.forEach((option, ind) => {
-      const optItem = optionCont(option, ind);
-      quesDiv.append(optItem);
-     // $('submit').click(function
-      // console.log(option)
-      // Add id Dynamically
-        let myInput=0, myLabel=0;
-        $(".opts").each(function(){
-          myInput+=1;
-          $(this).attr({
-            id:myInput
-          });
-        });
-        $(".optLabel").each(function(){
-          myLabel+=1;
-          $(this).attr("for", myLabel);
-        });
-      
-      let ansTextLabel = document.querySelectorAll('.optLabel');
-      // console.log($('.optLabel')[5];
-      //  let dataCount = 0;
+document.addEventListener("DOMContentLoaded", function(event) {     
+   // copy code here
+    const secQuiz = document.querySelector(".quiz");
+    const form = document.querySelector("#quiz_form");
+    const quizContDiv = document.querySelector(".quizContainer");
+    const quesDiv = document.querySelector('.quesContainer');
+    const mark = document.querySelector('.scoreCard .marks');
+    const outOfMarks = document.querySelector(".scoreCard .out-of-marks");
+    const hr = document.querySelector(".hr");
+    const btn = document.querySelector('.btn');
+    let score = 0;
   
-      for(let i=0;i < ansTextLabel.length; i++){
-        // console.log(ansTextLabel[5])
-        // ansTextLabel[i].setAttribute('onclick', "optionSelected(ansTextLabel)
-        ansTextLabel[i].setAttribute('onclick', optionSelected(ansTextLabel));
-      }
-      var dataCount = 0;
-      function optionSelected(ans){
-        let userAns = ans.textContent;
-        let correctAns = val.answer
-        console.log(userAns);
-        // console.log(correctAns);
-        // return userAns
-        if(userAns == correctAns) {
-          console.log('answer is correct');
-          //ans.classList.add("correct");
-        }else{
-          console.log('answer is wrong')
-          // ans.classList.add("incorrect");
+    // Fetch API
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET','https://5d76bf96515d1a0014085cf9.mockapi.io/quiz');
+    xhttp.send();
+    xhttp.addEventListener('readystatechange', onReadyStateChange);
+
+    function onReadyStateChange(e){
+        e.preventDefault();
+        if(e.target.readyState === 4 && e.target.status === 200){
+          const datum = JSON.parse(e.target.responseText);
+          // console.log(datum);
+          loadUI(datum);
+          
+          form.append(quizContDiv);
+          form.append(btn);
+          secQuiz.append(form);
+        
         }
-      }
-      // console.log(data[dataCount])
+    }
+
+  // App Creation
+  function loadUI(data){
+    data.forEach((val, ind, arr)=>{ 
+      let a = [];
+      let r = val.answer;
+      a.push(r);
+      // console.log(typeof r);
+      // answer from json
+    /* val.answer.forEach((ans) => {
+        console.log(ans);
+      }) */
       
-      // btn.addEventListener('click', () => {})
+      //Questions
+      const p = document.createElement('p');
+      p.classList.add("questions")
+      p.textContent = `Q${val.id}. ${val.question}` ;
+      quesDiv.append(p);
+      quizContDiv.append(quesDiv);
+      let answer = val.answer;
       
-    //   console.log(quesDiv);
-    //   
-    //   const scoreCardDiv = document.querSelector('.scoreCard')
+      // Options
+      val.options.forEach((option, ind) => {
+        const optItem = optionCont(option, ind);
+        quesDiv.append(optItem);
+      });
       
-    //   ansTextLabel.addEventListener('click', () => {
-    //     let mark = 0;
-    //     if(optIndex === val.answer){
-    //       mark+=1;
-    //     }else{
-    //       option == false;
-    //     }
-    //   });
+      // Line-Break
+      $("hr").addClass("hr")
+      $(quesDiv).append($("<hr />"));
+      
+      // Rendering Options
+      function optionCont(option, ind){
+        const divOpt = document.createElement("div");
+        divOpt.classList.add("options");
+        divOpt.classList.add(`option${ind}`);
+        // optionSelected(answer,ind)
+        // console.log(ind)
+        divOpt.insertAdjacentHTML('afterbegin', `
+          <label for=${ind} class="optLabel">
+          <input type="radio" id=${ind} value=${option} name="check_${val.id}" class="opts"></input>
+          ${option}
+          </label>`);
+        return divOpt;      
+      };
     });
     
-
-    $(quesDiv).append($("<hr />"));
-    
-/***********************************************************
-    console.log(val.options.length)
-    indexes= val.options.length*5
-    for(let i=0; i<indexes; i++){
-        $('input').attr('id', `${indexes}`)
-    }
-     */
-
-    function optionCont(option, ind){
-      const divOpt = document.createElement("div");
-      divOpt.classList.add("option");
-      divOpt.insertAdjacentHTML('afterbegin', `
-        <label class="optLabel">
-        <input type="radio" name="check_${val.id}" class="opts"></input>
-        ${option}
-        </label>
-      `);
-      return divOpt;
-   }
-    
-});
-
-  function scoreCounter(mark){
-        const scoreTotal = document.querySelector('.marks');
-        mark++;
-        scoreTotal.textContent = `${mark}/${data.length}`
+    // Submit btn functionality
+    const btn = document.querySelector(".btn");
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const checkedAnswer = optionSelected();
+      
+        //console.log(data[i].answer);
+      // console.log(typeof data[0].answer); -> Number
+      // console.log(typeof checkedAnswer[0]); -> String
+      // console.log(checkedAnswer); 
+      // console.log(typeof checkedAnswer); -> Object
+      // console.log(checkedAnswer[0]); 
+      
+        for(let i=0; i<data.length; i++){
+              const checkedAnsNum = Number(checkedAnswer[i]);
+          if(checkedAnsNum === data[i].answer){
+            score++;
+            // console.log(score);
+          }
+          updateScoreBoard();
+          e.target.form.style.display='none';
+        };
+          /* if(e.target.getAttribute('name').value){
+          optionSelected(e.target.getAttribute('name').value)
+          }else{} */
+      });
+    return quizContDiv;
   };
-  return quizContDiv;
-};
- 
+  // User Selected Option
+  function optionSelected(){
+      let userAns = [];
+      const ansInput = document.querySelectorAll(".opts");
+        for(const currAnsElem of ansInput){
+          if(currAnsElem.checked){
+            userAns.push(currAnsElem.id);
+          }  
+        };  
+    return userAns;
+  };
 
+  function updateScoreBoard(){
+    mark.innerText = score;
+  };
 
-
-
+});
+  
+    
+  
